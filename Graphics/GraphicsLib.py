5b75b933489efa551a08d2822c70c6e2bd541632
austinrosh <austin457@gmail.com>
2016-06-24 15:04:25 -0400

    ignore

--- Graphics/GraphicsLib.py
+++ Graphics/GraphicsLib.py
@@ -1,6 +1,6 @@
-#import pygame
-#from Game_Boarder import *
-#from Wizard import *
-#from Enemy import *
+import pygame
+from Game_Boarder import *
+from Wizard import *
+from Enemy import *
 
 
--- NewStartScreen.py
+++ NewStartScreen.py
@@ -15,21 +15,25 @@ def render_text(word, x, y, font, color): #Function to render text. includes spe
     my_font = pygame.font.SysFont('arial',60)
     label = my_font.render(word, 1, (color))
     screen.blit(label, (x, y))
+    label.set_colorkey(black)
 
 def render_smaller_text(word, x, y, font, color): #Function to render smaller text.
     my_small_font = pygame.font.SysFont('arial', 36)
     new_label = my_small_font.render(word, 1, (color))
+    new_label.set_colorkey(black)
     screen.blit(new_label, (x, y))
 
 def render_title_text(word, x, y, font, color): #Renders title text
     my_title_font = pygame.font.SysFont('arial', 80)
     title_label = my_title_font.render(word, 1, (color))
     screen.blit(title_label, (x, y))
+    title_label.set_colorkey(black)
 
 if rendertextvariable == True: #Referring back to that statement from earlier to print the text.
     render_title_text("Sample Title", 210, 20, "arial", red)
     render_text("Start Game", 260, 160, "arial", red)
     render_smaller_text("Instructions", 200, 320, "arial", red)
     render_smaller_text("Credits", 480, 320, "arial", red)
-    
+
+
 pygame.display.flip()
