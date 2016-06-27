4c756d5eee768c1388e51b3e986c1e66de8ae816
Andrew99B <lightningbird99@gmail.com>
2016-06-27 10:50:49 -0400

    A slight change to the Enemy AI function

--- Enemy AI.py
+++ Enemy AI.py
@@ -1,15 +1,19 @@
 import pygame
 import time
 import random
-import GraphicsLib
+
+class Enemynw:
+    def __init__(self):
+        self.lives=resil
+        self.entyp=mtype
+        self.wpn=mweapon
+        self.xpos=xstrt
+        self.ypos=ystrt
+        self.enm
 
 class Enms:
     def __init__ (self):
-        self.lives=[]
         self.enemies=[]
-        self.wpn=[]
-        self.xpos=[]
-        self.ypos=[]
         self.resil=1
     def spawndcd(difficulty):
         EnLst=["grunt"]                             #Current list of all enemies except bosses
@@ -19,7 +23,7 @@ class Enms:
         seed=random.randint(1,5)                    #Determines the number of enemies spawned, which is then further modulated by the difficulty.
         Etype=EnLst[whm]
         Ewpn=WpnLst[wpn]
-        num=seed+(difficulty//1)                    #For this to work difficulty must always be an integer
+        num=seed+(difficulty//1)
         xval=800                                    #enum is the number of enemies
         ymod=(500/(num+1))                          #ymod is the value which you must multiply by each ship's number to get it's position.     Change this if y changes from 500.
         return(Etype, Ewpn, num, xval, ymod, difficulty)
@@ -27,26 +31,17 @@ class Enms:
         self.resil=self.resil+(diff//5)
         if (diff//5)>=1:
             diff=0
-        self.lives.append(resil)
-        self.enemies.append(mtype)
-        self.wpn.append(mweapon)
-        self.xpos.append(xstrt)
-        self.ypos.append(ystrt)
-        self.enemies.append(mtype)
+        Newen=Enemynw()
+        self.enemies.append(Newen)
     def check (self, pblltlst):
         bulltnum=0
         for blt in pblltlst:
             enemynum=0
             for enmy in enmylst:
-                if pygame.Rect.colliderect(pbltlst[bulltnum], self.enemies[enemynum])!=False:
-                    self.lives[enemynum]-=1
-                    if self.lives[enemynum]<=0:
-                        self.lives.remove(self.lives[enemynum])
-                        self.enemies.remove(self.lives[enemynum])
-                        self.wpn.remove(self.lives[enemynum])
-                        self.xpos.remove(self.lives[enemynum])
-                        self.ypos.remove(self.lives[enemynum])
-                        self.resil.remove(self.lives[enemynum])
+                if pygame.Rect.colliderect(pbltlst[bulltnum], self.enemies[enemynum].enm)!=False:
+                    self.enemies[enemynum].lives-=1
+                    if self.enemies[enemynum].lives<=0:
+                        self.enemies.remove(self.enemies[enemynum])
                         enemynum-=1
                     pbltlst.remove(pbltlst[bulltnum])      #removes player shots which hit the enemy, post damage.
                     bulltnum-=1
@@ -55,31 +50,31 @@ class Enms:
     def emovement (self, ennum, movedi):
         strtx=0
         strty=0
-        if (self.xpos[ennum]>=100):
+        if (self.enemies[ennum].xpos>=100):
             strtx=strtx-1
-        if ((self.ypos[ennum]>0)&(movedi="UP")):
+        if ((self.enemies[ennum].ypos>0)&(movedi="UP")):
             strty=strty-1
-        elif ((self.ypos[ennum]<=0)&(movedi="UP")):
+        elif ((self.enemies[ennum].ypos<=0)&(movedi="UP")):
             movedi="DOWN"
-        elif ((self.ypos[ennum]<500)&(movedi="DOWN")):
+        elif ((self.enemies[ennum].ypos<500)&(movedi="DOWN")):
             strty=strty+1
-        elif ((self.ypos[ennum]>=500)&(movedi="DOWN")):
+        elif ((self.enemies[ennum].ypos>=500)&(movedi="DOWN")):
             movedi="UP"
-        self.ypos[ennum]+=strty
-        self.xpos[ennum]+=strtx
+        self.enemies[ennum].ypos+=strty
+        self.enemies[ennum].xpos+=strtx
     def fire (self, ennum, plrx, plry):
-        ammox=self.xpos[ennum]
-        ammoy=self.ypos[ennum]
-        if self.wpn[ennum]== "Firebolt":
+        ammox=self.enemies[ennum].xpos
+        ammoy=self.enemies[ennum].ypos
+        if self.enemies[ennum].wpn== "Firebolt":
             velox=1
             veloy=0
             numb=1
-        elif self.wpn[ennum]== "Fireblast":
+        elif self.enemies[ennum].wpn== "Fireblast":
             velox=[1,1,1,1,1]
             veloy=[ 1,.5,0,-.5,-1]
             numb=5
-        elif self.wpn[ennum]== "Aimfire":
-            velox=(plrx-self.xpos[ennun])
-            veloy=(plry-self.ypos[ennun])
+        elif self.wpn[ennum].wpn== "Aimfire":
+            velox=(plrx-self.enemies[ennun].xpos)
+            veloy=(plry-self.enemies[ennun].xpos)
             numb=1
         return(velox, veloy)
